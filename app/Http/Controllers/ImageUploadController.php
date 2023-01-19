4a704e60ac156be1010708bfaeee9f9f19f2a1d7
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2023-01-20 01:23:50 +0600

    extra bug fix

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -15,14 +15,39 @@ class ImageUploadController extends Controller
 
     public function file_store(Request $request)
     {
+
         $image = $request->file('file');
         $imageName = $image->getClientOriginalName();
+
+
+        if (ImageUpload::where('filename', $imageName)->exists()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Image already exists',
+                'data' => \null,
+            ]);
+        }
 
         $image->move(public_path('images'), $imageName);
 
         $imageUpload = new ImageUpload();
         $imageUpload->filename = $imageName;
         $imageUpload->save();
-        return response()->json(['success' => $imageName]);
+        return response()->json([
+            'status' => true,
+            'message' => 'Image uploaded successfully',
+            'data' => asset('img/' . $imageName)
+        ]);
+    }
+
+
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function showAll()
+    {
+        return \response()->json(ImageUpload::latest('id')->paginate(20));
     }
 }
